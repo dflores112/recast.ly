d46f17e4d3d819d3d35e175dfb8ca4f1ab7e5503
Daniel Flores <danielflores125@gmail.com>
2020-09-01 11:04:09 -0700

    Rendered videolist fixed errors

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,3 +1,11 @@
+import exampleVideoData from '../data/exampleVideoData.js';
+console.log(exampleVideoData);
+
+import VideoList from './VideoList.js';
+import VideoPlayer from './VideoPlayer.js';
+import Search from './Search.js';
+
+
 var App = () => (
   <div>
     <nav className="navbar">
@@ -10,7 +18,7 @@ var App = () => (
         <div><h5><em>videoPlayer</em> view goes here</h5></div>
       </div>
       <div className="col-md-5">
-        <div><h5><em>videoList</em> view goes here</h5></div>
+        <VideoList data={exampleVideoData} />
       </div>
     </div>
   </div>
@@ -19,3 +27,16 @@ var App = () => (
 // In the ES6 spec, files are "modules" and do not share a top-level scope
 // `var` declarations will only exist globally where explicitly defined
 export default App;
+
+
+/*
+app tree
+
+App
+  |__Search
+  |__VideoList
+  |   |__VideoListEntry
+  |__VideoPlayer
+
+
+*/
--- src/components/VideoList.jsx
+++ src/components/VideoList.jsx
@@ -1,4 +1,6 @@
-var VideoList = () => (
+import VideoListEntry from './VideoListEntry.js';
+
+var VideoList = (props) => (
   <div className="video-list">
     <div><h5><em>videoListEntry</em> view goes here</h5></div>
     <div><h5><em>videoListEntry</em> view goes here</h5></div>
@@ -13,7 +15,6 @@ var VideoList = () => (
 VideoList.propTypes = {
   videos: PropTypes.array.isRequired
 };
-
 // In the ES6 spec, files are "modules" and do not share a top-level scope.
 // `var` declarations will only exist globally where explicitly defined.
 export default VideoList;
