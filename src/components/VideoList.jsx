637f86c64921daa1d0e09c20ca350a6b075c60ff
James Scolamieri <james.scolamieri@gmail.com>
2020-09-01 21:09:18 -0700

    added packages and updated video list

--- src/components/VideoList.jsx
+++ src/components/VideoList.jsx
@@ -19,43 +19,6 @@ var VideoList = (props) => (
   </div>
 );
 
-/*
-props = {
-  videos: {this.state.availableVidoes},
-  playThisVideo: playThisVideo
-}
-*/
-
-// - in list entry, use correct video thumbnail from data set
-// - in list entry, add video description
-
-// var VideoList = (props) => (
-//   <div className="video-list">
-//     <div><h5><em>videoListEntry</em> view goes here</h5></div>
-//     <div><h5><em><VideoListEntry video={props.videos[0]} /></em> view goes here</h5></div>
-//     <div><h5><em><VideoListEntry video={props.videos[1]} /></em> view goes here</h5></div>
-//     <div><h5><em><VideoListEntry video={props.videos[2]} /></em> view goes here</h5></div>
-//     <div><h5><em><VideoListEntry video={props.videos[3]} /></em> view goes here</h5></div>
-//     <div><h5><em><VideoListEntry video={props.videos[4]} /></em> view goes here</h5></div>
-//   </div>
-// );
-
-// class VideoList extends React.Component {
-//   constructor(props) {
-//     super(props);
-//   }
-
-//   render() {
-
-//     return (
-//       <div videotitle={this.props.videos[0].snippet.title}>{this.props.videos[0].snippet.title}</div>
-//     );
-//   }
-// }
-
-
-// exampleVideoData.snippet.title
-
 // PropTypes tell other developers what `props` a component expects
 // Warnings will be shown in the console when the defined rules are violated
 VideoList.propTypes = {
