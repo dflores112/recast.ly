21be9e3c706a8a064e2c268d90f6624d3d2d1652
James Scolamieri <james.scolamieri@gmail.com>
2020-09-01 20:56:26 -0700

    resolved conflicts2

--- src/index.js
+++ src/index.js
@@ -1,9 +1,3 @@
 // TODO: Render the `App` component to the DOM
 import App from './components/App.js';
-
-<<<<<<< HEAD
-ReactDOM.render(<App />, document.getElementById('app'));
-=======
-
 ReactDOM.render(<App/>, document.getElementById('app'));
->>>>>>> 80b8aa689f9c1aec64359c643465fa1dbf398b9e
